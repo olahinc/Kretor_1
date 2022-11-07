038da8da05fad73032589a8a27207f6cfd0313cd
Aleksandra <aleksandra.hinc00@gmail.com>
2022-11-07 12:00:36 +0100

    Get, set

---
+++ src/Hero.java
@@ -0,0 +1,51 @@
+public class Hero {
+    private String name;
+    private String clas;
+    private int powerpoints;
+    private int manapoints;
+    private boolean newcharacter;
+
+    Hero(String name, String clas, int powerpoints, int manapoints, boolean newcharacter) {
+        this.name = name;
+        this.clas = clas;
+        this.powerpoints = powerpoints;
+        this.manapoints = manapoints;
+        this.newcharacter = newcharacter;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String setName(String name) {
+        this.name = name;
+        return name;
+    }
+    public String getClas() {
+        return this.clas;
+    }
+    public String setClas(String clas) {
+        this.clas = clas;
+        return clas;
+    }
+    public int getPowerpoints() {
+        return this.powerpoints;
+    }
+    public int setPowerpoints(int powerpoints) {
+        this.powerpoints = powerpoints;
+        return powerpoints;
+    }
+    public int getManapoints() {
+        return this.manapoints;
+    }
+    public int setManapoints(int manapoints) {
+        this.manapoints = manapoints;
+        return manapoints;
+    }
+    public boolean getNewcharacter() {
+        return this.newcharacter;
+    }
+    public boolean setNewcharacter(boolean newcharacter) {
+        this.newcharacter = newcharacter;
+        return newcharacter;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Hero Hero1=new Hero("Galadriela", "druga", 10, 10, true);
+    }
+}
